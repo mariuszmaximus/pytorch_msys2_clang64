851879947697c4b357a92e943b48ee43433dfdef
mariuszmaximus <mariusz.maximus@gmail.com>
2025-02-01 22:30:21 +0100

    msys2: disable CUDA_KERNEL_ASSERT undefined symbol: __assert_fail

--- c10/test/util/exception_test.cpp
+++ c10/test/util/exception_test.cpp
@@ -34,7 +34,7 @@ TEST(ExceptionTest, TORCH_INTERNAL_ASSERT_DEBUG_ONLY) {
 }
 
 // On these platforms there's no assert
-#if !defined(__ANDROID__) && !defined(__APPLE__)
+#if !defined(__ANDROID__) && !defined(__APPLE__) && !defined(MSYS2)
 TEST(ExceptionTest, CUDA_KERNEL_ASSERT) {
   // This function always throws even in NDEBUG mode
   ASSERT_DEATH_IF_SUPPORTED({ CUDA_KERNEL_ASSERT(false); }, "Assert");
